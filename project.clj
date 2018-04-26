14a3a6f73c6589b5d5c467b6ce00f44c33f8638a
Dan Gopstein <dan@gopstein.com>
2018-04-26 13:47:04 -0400

    Update eclipse libraries

--- project.clj
+++ project.clj
@@ -7,10 +7,13 @@
                  [org.clojure/test.check "0.9.0"]
                  [prismatic/schema "1.1.3"]
                  [swiss-arrows "1.0.0"]
-                 [org.eclipse.core/org.eclipse.core.resources "3.6.0.v20100526-0737"]
                  ]
-  :resource-paths [;"resources/org.eclipse.cdt.core_6.4.0.201802122019.jar"
-                   "resources/org.eclipse.cdt.core_6.2.0.201612061315.jar"
+  :resource-paths ["./resources/org.eclipse.cdt.core_6.4.0.201803220800.jar"
+                   "./resources/org.eclipse.core.resources_3.12.0.v20170417-1558.jar"
+                   "./resources/org.eclipse.core.runtime_3.13.0.v20170207-1030.jar"
+                   "./resources/org.eclipse.equinox.common_3.9.0.v20170207-1454.jar"
+                   "./resources/org.eclipse.osgi_3.12.100.v20180210-1608.jar"
+                   "./resources/org.eclipse.core.jobs_3.9.3.v20180115-1757.jar"
                    "src/test/resources"
                    ]
   :java-source-paths ["src/java"]
